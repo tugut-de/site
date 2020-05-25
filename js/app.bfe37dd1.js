267ddeb0e65a4ae4a3b8f904ca0d4c6d9b509661
Axel Hodler <axel@hodler.co>
2020-05-25 11:59:25 +0200

    extend FAQ

--- js/app.bfe37dd1.js
+++ js/app.bfe37dd1.js
@@ -1,16 +1,96 @@
 (function(e){function t(t){for(var n,a,c=t[0],i=t[1],l=t[2],f=0,s=[];f<c.length;f++)a=c[f],Object.prototype.hasOwnProperty.call(o,a)&&o[a]&&s.push(o[a][0]),o[a]=0;for(n in i)Object.prototype.hasOwnProperty.call(i,n)&&(e[n]=i[n]);p&&p(t);while(s.length)s.shift()();return u.push.apply(u,l||[]),r()}function r(){for(var e,t=0;t<u.length;t++){for(var r=u[t],n=!0,c=1;c<r.length;c++){var i=r[c];0!==o[i]&&(n=!1)}n&&(u.splice(t--,1),e=a(a.s=r[0]))}return e}var n={},o={app:0},u=[];function a(t){if(n[t])return n[t].exports;var r=n[t]={i:t,l:!1,exports:{}};return e[t].call(r.exports,r,r.exports,a),r.l=!0,r.exports}a.m=e,a.c=n,a.d=function(e,t,r){a.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},a.r=function(e){"undefined"!==typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},a.t=function(e,t){if(1&t&&(e=a(e)),8&t)return e;if(4&t&&"object"===typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(a.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var n in e)a.d(r,n,function(t){return e[t]}.bind(null,n));return r},a.n=function(e){var t=e&&e.__esModule?function(){return e["default"]}:function(){return e};return a.d(t,"a",t),t},a.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},a.p="/";var c=window["webpackJsonp"]=window["webpackJsonp"]||[],i=c.push.bind(c);c.push=t,c=c.slice();for(var l=0;l<c.length;l++)t(c[l]);var p=i;u.push([0,"chunk-vendors"]),r()})({0:function(e,t,r){e.exports=r("56d7")},"56d7":function(e,t,r){"use strict";r.r(t);r("e260"),r("e6cf"),r("cca6"),r("a79d");var n=r("2b0e"),o=function(){var e=this,t=e.$createElement,r=e._self._c||t;return r("div",{attrs:{id:"app"}},[r("VueFaqAccordion",{attrs:{items:e.myItems,"border-color":"#50DD93","active-color":"#50DD93"}})],1)},u=[],a=r("744c"),c={name:"App",components:{VueFaqAccordion:a["a"]},data:function(){return{
   myItems:[
     {
-      title: "Frage1 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labor",
-      value: "Antwort1: et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolor"
+      title: "Was ist eine Lotterie?",
+      value: "Bei einer Lotterie handelt es sich um eine Ausspielung, die auf einem bestimmten Spielplan gegen einen festgelegten Einsatz für bestimmte Geld- oder Sachgewinne beruht. Teilnehmer/innen haben eine Chance auf die Gewinne. In Deutschland erlaubt die Gesetzgebung Vereinen ausdrücklich Lotterien innerhalb vorgegebener Rahmenbedingungen durchzuführen."
     },
     {
-      title: "Frage2",
-      value:"Antwort2"
+      title: "Was ist eine Soziallotterie?",
+      value: "Bei einer Soziallotterie stehen soziale Projekte, Vereine und Organisationen im Vordergrund. Durch die Lotterie eingenommene Spielerträge kommen insbesondere diesen zu Gute. Die Aktion Mensch ist z.B. eine der bekanntesten Soziallotterien in Deutschland."
     },
     {
-      title:"Frage3",
-      value:"Antwort3"
+      title: "Was ist die rechtliche Grundlage?",
+      value: "Die gesetzliche Grundlage zur Durchführung der tugut Lotterie für Vereine basiert auf § 12 des Ausführungsgesetzes zum Glücksspielstaatsvertrags und dem jeweiligen Landesglücksspielgesetz. In Baden-Württemberg ist dieses z.B. durch die Bekanntmachung des Regierungspräsidiums Karlsruhe über die allgemeine Erlaubnis für öffentliche Lotterien und Ausspielungen geregelt."
+    },
+    {
+      title: "Wie sind die finanziellen Rahmenbedingungen?",
+      value: "Einzelne Lotterien können einen Umsatz (Ergebnis aus Anzahl der verkauften Lose * Preis Los) von bis zu 40.000 Euro betragen. Aus den Losverkäufen werden Gewinne für Teilnehmer/innen und Kosten für die Durchführung verbucht. Je nach Ausgestaltung verbleiben Vereinen bis zu 75% des Umsatzes einer Lotterie. Mindestens 25% des Umsatzes muss zur Gewinnausschüttung bereitgestellt werden."
+    },
+    {
+      title: "Sind die Einnahmen der Ausspielung für den Verein steuerfrei?",
+      value: "Alle Einnahmen der Ausspielung sind potentiell für den Verein steuerfrei. Hierzu kann eine Steuerbefreiung beim Finanzamt beantragt werden. Die tugut GmbH übernimmt die Anmeldung und Beantragung auf Steuerbefreiung beim Finanzamt."
+    },
+    {
+      title: "Wie oft kann mein Verein eine Lotterie durchführen?",
+      value: "Vereine und gemeinnützige Organisationen können bis zu zwei Lotterien pro Jahr durchführen. Die Verwendung der Spielerlöße (Reinertrag) muss einem spezifischen Verwendungszweck dienen."
+    },
+    {
+      title: "Wie und wo erfolgt die Lotterie?",
+      value: "Die Ausspielung erfolgt über den Verein und ausgewählten Mitglieder, die damit beauftragt werden, die Ausspielung durchzuführen. tugut stellt Vereinen alle benötigten Unterlagen zur Ausspielung bereit."
+    },
+    {
+      title: "Wie lange dauert die Durchführung einer Lotterie?",
+      value: "Die Durchführung einer tugut Lotterie, d.h. Start des Losverkaufs bis zum Tag der Ziehung, kann maximal 2 Monate betragen. Innerhalb dieses Zeitraums können Mitglieder, Geschäftspartner und die Öffentlichkeit informiert werden und an der Lotterie teilnehmen."
+    },
+    {
+      title: "Wie werden Lose ver- bzw. gekauft?",
+      value: "Lose werden von ausgewählten Mitgliedern verkauft. Teilnehmer/innen erwerben mit dem Kauf ein Los für die jeweilige Ziehung und werden in einer Spiel-Tabelle eingetragen. Die ausgewählten Mitglieder, die mit der Durchführung der Lotterie beauftragt sind, nehmen Geld entgegen und führen die Kasse der Lotterie."
+    },
+    {
+      title: "Wie erfolgt die Benachrichtigung der Gewinner?",
+      value: "Die Benachrichtigung erfolgt über eine extra bereitgestellte Webseite, die tugut für den Verein anlegt. Zudem kann der Verein über seine Kommunikationskanäle die Ziehungsergebnisse veröffentlichen."
+    },
+    {
+      title: "Wie erfolgt die Gewinnausschüttung an die Gewinner?",
+      value: "Der Verein bzw. die Mitglieder sind für die Gewinnausschüttung zuständig. Gewinner können ihren Gewinn an dem Ort der Kassenführung (z.B. im Vereinsheim) bzw. über die ausgewählten Mitglieder einfordern."
+    },
+    {
+      title: "Wie werden die Gewinnzahlen ermittelt?",
+      value: "Die Gewinnzahlen werden für jede Ziehung maschinell mit modernen Zufallszahlgeneratoren durch tugut ermittelt."
+    },
+    {
+      title: "Welche Wahrscheinlichkeiten gibt es bei der Lotterie-Ziehung?",
+      value: "Die Anzahl der Lose, Preise und entsprechenden Gewinnpläne werden individuell an die Bedürfnisse und Möglichkeiten des Vereins angepasst."
+    },
+    {
+      title: "Wer trägt die Veranstaltungskosten?",
+      value: "Es fallen für den Verein keine Kosten an. Der Aufwand ist minimal und beläuft sich lediglich auf die zeitliche Vorbereitung für die Lotterie und das Engagement der mitwirkenden Vereinsmitglieder."
+    },
+    {
+      title: "Welche Art von Organisationen dürfen eine öffentliche Ausspielungen durchführen?",
+      value: "Folgende Organisationen sind durch das Gesetz berechtigt öffentliche Ausspielungen durchzuführen:  \n" +
+        "\n" +
+        " \n" +
+        "\n" +
+        "Rechtsfähige gemeinnützige Vereine, Stiftungen, Organisationen der Wohlfahrtspflege, Organisationen der Jugendhilfe und Jugendpflege, Kirchengemeinden und Religionsgemeinschaften, Feuerwehren, juristische Personen des öffentlichen Rechts, Organisationen von politischen Vereinen, gewerkschaftliche Organisationen."
+    },
+    {
+      title: "Wie erfolgen Planung und Veranstaltung der Ausspielung?",
+      value: "Das Dienstleistungsangebot der tugut GmbH umfasst alle Aspekte zur Planung und Veranstaltung der Ausspielung. Diese beinhalten die initiale Beratung, Vorschläge zur Aufstellung des Spiel- und Gewinnplans, Bereitstellung der Lose, Unterstützung der Anmeldung beim Finanzamt und die Ziehung als auch Veröffentlichung der Gewinner. Der Verkauf der Lose, und die Gewinnausschüttung erfolgen über den Verein. Gemeinsam erarbeiten wir ein individuelles Konzept nach den Bedürfnissen und Möglichkeiten des Vereins."
+    },
+    {
+      title: "Wer ist für die Durchführung verantwortlich?",
+      value: "Offizieller Veranstalter der Lotterie ist der Verein bzw. die gemeinnützige Organisation selbst. Die tugut GmbH berät und unterstützt als Dienstleister über die gesamte Durchführung der Lotterie hinweg."
+    },
+    {
+      title: "Wie lange dauert es von Antrag bis zur Veranstaltung?",
+      value: "Von der Antragstellung bei der tugut GmbH bis zum Start des Losverkaufs ist mit 2-4 Wochen Vorlauf zu rechnen. Zu Anfang wird die Konzeption der Veranstaltung gemeinsam erstellt und es erfolgt die Anmeldung beim Finanzamt."
+    },
+    {
+      title: "Mit wieviel Aufwand ist für Vereine zu rechnen?",
+      value: "Das Dienstleistungsangebot der tugut GmbH sieht vor alle Aspekte der Planung und Durchführung für die Lotterie so weit wie möglich zu übernehmen bzw. vorzubereiten. Für die richtige Konzeption der Veranstaltung an die Bedürfnisse und Möglichkeiten des Vereins ist mit einem initialen Beratungsaufwand zu rechnen. Die tugut GmbH unterstützt Vereine auch bei der Information über die Lotterie gegenüber Mitgliedern, Geschäftspartnern und der Öffentlichkeit."
+    },
+    {
+      title: "Wie informiere ich meine Mitglieder/innen über die Lotterie?",
+      value: "Mitglieder, Geschäftspartner und die Öffentlichkeit können über die etablierten Kommunikationskanäle des Vereins informiert werden. Diese können digitale Medien, wie Informationen auf der Webseite des Vereins, E-Mail-Verteiler oder soziale Medien, wie Facebook, Linkedin und Instagram umfassen. Zudem können auch traditionelle Informationsmaßnahmen, wie Plakate, Inserate und Flyer gewählt werden. Die tugut GmbH unterstützt aktiv bei der Gestaltung und Aufbereitung der Information durch standardisierte Vorlagen."
+    },
+    {
+      title: "Welche Daten werden von Teilnehmer/innen gespeichert?",
+      value: "Für die Ausspielung werden lediglich die Namen der Spielteilnehmer in einer Liste geführt. Hiermit wird sichergestellt, dass Los-Nummer und Spielteilnehmer manipulationssicher bis zur Gewinnausschüttung zurückverfolgt werden können. Die Liste wird ausschließlich über den Verein zum Zweck der Lotterie geführt. Nach vollendeter Gewinnausschüttung kann die Spiel-Tabelle vernichtet werden. Die tugut GmbH erhält zu keiner Zeit Daten der Spielteilnehmer."
+    },
+    {
+      title: "Wie stelle ich einen Antrag?",
+      value: "Sie können jederzeit einen Antrag stellen. Bitte füllen Sie dazu einfach das Anfrage Formular aus oder kontaktieren uns per Telefon."
     }
   ]}}},i=c,l=r("2877"),p=Object(l["a"])(i,o,u,!1,null,null,null),f=p.exports;n["a"].config.productionTip=!1,new n["a"]({render:function(e){return e(f)}}).$mount("#app")}});
 //# sourceMappingURL=app.bfe37dd1.js.map
