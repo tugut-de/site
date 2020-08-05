1c6601372a958b069a8758dfb383f29888e19827
Axel Hodler <axel@hodler.co>
2020-08-05 20:18:17 +0200

    fix spacing

--- js/app.kocherlager.js
+++ js/app.kocherlager.js
@@ -1,7 +1,7 @@
 (function(e){function t(t){for(var n,a,c=t[0],i=t[1],l=t[2],f=0,s=[];f<c.length;f++)a=c[f],Object.prototype.hasOwnProperty.call(o,a)&&o[a]&&s.push(o[a][0]),o[a]=0;for(n in i)Object.prototype.hasOwnProperty.call(i,n)&&(e[n]=i[n]);p&&p(t);while(s.length)s.shift()();return u.push.apply(u,l||[]),r()}function r(){for(var e,t=0;t<u.length;t++){for(var r=u[t],n=!0,c=1;c<r.length;c++){var i=r[c];0!==o[i]&&(n=!1)}n&&(u.splice(t--,1),e=a(a.s=r[0]))}return e}var n={},o={app:0},u=[];function a(t){if(n[t])return n[t].exports;var r=n[t]={i:t,l:!1,exports:{}};return e[t].call(r.exports,r,r.exports,a),r.l=!0,r.exports}a.m=e,a.c=n,a.d=function(e,t,r){a.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},a.r=function(e){"undefined"!==typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},a.t=function(e,t){if(1&t&&(e=a(e)),8&t)return e;if(4&t&&"object"===typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(a.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var n in e)a.d(r,n,function(t){return e[t]}.bind(null,n));return r},a.n=function(e){var t=e&&e.__esModule?function(){return e["default"]}:function(){return e};return a.d(t,"a",t),t},a.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},a.p="/";var c=window["webpackJsonp"]=window["webpackJsonp"]||[],i=c.push.bind(c);c.push=t,c=c.slice();for(var l=0;l<c.length;l++)t(c[l]);var p=i;u.push([0,"chunk-vendors"]),r()})({0:function(e,t,r){e.exports=r("56d7")},"56d7":function(e,t,r){"use strict";r.r(t);r("e260"),r("e6cf"),r("cca6"),r("a79d");var n=r("2b0e"),o=function(){var e=this,t=e.$createElement,r=e._self._c||t;return r("div",{attrs:{id:"app"}},[r("VueFaqAccordion",{attrs:{items:e.myItems,"border-color":"black","active-color":"white"}})],1)},u=[],a=r("744c"),c={name:"App",components:{VueFaqAccordion:a["a"]},data:function(){return{
   myItems:[
     {
-      title: "Wie erfolgt die Benachrichtigung der Gewinner?",
+      title: "Wie erfolgt die Benachrichtigung der Gewinner?",
       value: "Die Benachrichtigung über die Ziehungsergebnisse der Lotterie erfolgt hier auf der Webseite am 02.08.2020 um 18 Uhr. Zudem werden die Apis über einzelne Kommunikationskanäle über die Ziehungsergebnisse informieren."
     },
     {
@@ -13,16 +13,16 @@
         "Bitte schicken Sie ihr Los dazu an Gewinner@tugut.de. tugut benachrichtigt darauf den Verein. Für Fragen stehen wir Ihnen gerne per Telefon unter 01728441452 zur Verfügung."
     },
     {
-      title: "Wie erfolgt die Gewinnausschüttung an die Gewinner?",
+      title: "Wie erfolgt die Gewinnausschüttung an die Gewinner?",
       value: "Der Verein ist für die Gewinnausschüttung zuständig. Gewinner werden entweder in Bar oder per Überweisung durch den Verein ausbezahlt. Hierzu wird der Verein direkten Kontakt mit dem Gewinner aufnehmen."
     },
     {
       title: "Was ist eine Lotterie?",
-      value: "Bei einer Lotterie handelt es sich um eine Ausspielung, die auf einem bestimmten Spielplan gegen einen festgelegten Einsatz für bestimmte Geld- oder Sachgewinne beruht. Teilnehmer/innen haben eine Chance auf die Gewinne. In Deutschland erlaubt die Gesetzgebung Vereinen ausdrücklich Lotterien innerhalb vorgegebener Rahmenbedingungen durchzuführen."
+      value: "Bei einer Lotterie handelt es sich um eine Ausspielung, die auf einem bestimmten Spielplan gegen einen festgelegten Einsatz für bestimmte Geld- oder Sachgewinne beruht. Teilnehmer/innen haben eine Chance auf die Gewinne. In Deutschland erlaubt die Gesetzgebung Vereinen ausdrücklich Lotterien innerhalb vorgegebener Rahmenbedingungen durchzuführen."
     },
     {
       title: "Was ist eine Soziallotterie?",
-      value: "Bei einer Soziallotterie stehen soziale Projekte, Vereine und Organisationen im Vordergrund. Durch die Lotterie eingenommene Spielerträge kommen insbesondere diesen zu Gute. Die Aktion Mensch ist z.B. eine der bekanntesten Soziallotterien in Deutschland."
+      value: "Bei einer Soziallotterie stehen soziale Projekte, Vereine und Organisationen im Vordergrund. Durch die Lotterie eingenommene Spielerträge kommen insbesondere diesen zu Gute. Die Aktion Mensch ist z.B. der bekanntesten Soziallotterien in Deutschland."
     },
     {
       title: "Was ist die rechtliche Grundlage?",
@@ -34,7 +34,7 @@
     },
     {
       title: "Wer ist für die Durchführung verantwortlich?",
-      value: "Offizieller Veranstalter der Lotterie ist der Verein bzw. die gemeinnützige Organisation selbst. Die tugut GmbH berät und unterstützt als Dienstleister über die gesamte Durchführung der Lotterie hinweg."
+      value: "Offizieller Veranstalter der Lotterie ist der Verein bzw. die gemeinnützige Organisation selbst. Die tugut berät und unterstützt als Dienstleister über die gesamte Durchführung der Lotterie hinweg."
     },
     {
       title: "Wie sieht der Gewinnplan dieser Lotterie aus?",
